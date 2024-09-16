66a78279f9d8f061850ced78f8caaac0d2061d56
Nicholas Emery <108698936+NicholasEmery@users.noreply.github.com>
2024-09-16 10:45:41 -0300

    Update create.py

--- app/routes/create.py
+++ app/routes/create.py
@@ -1,6 +1,6 @@
 from flask import request, jsonify, current_app
 from app import db
-from app.models import Manifestacao
+from app.models import Manifestacao, Usuario
 
 @current_app.route('/manifestacao', methods=['POST'])
 def criar_manifestacao():
@@ -16,3 +16,21 @@ def criar_manifestacao():
     db.session.commit()
 
     return jsonify({"mensagem": "Manifestação criada com sucesso", "id": manifestacao.id}), 201
+
+@current_app.route('/usuario', methods=['POST'])
+def criar_usuario():
+    dados = request.get_json()
+    user = dados.get('nome')
+    password = dados.get('senha')
+
+    if not user or not password:
+        return jsonify({"erro": "Nome e senha são obrigatórios"}), 400
+    
+    if len(password) > 8:
+        return jsonify({"erro": "Senha só pode ter até 8 caracteres"}), 400
+
+    usuario = Usuario(user=user, password=password)
+    db.session.add(usuario)
+    db.session.commit()
+
+    return jsonify({"server": "Usuario criado com sucesso", "id": usuario.id}), 201
